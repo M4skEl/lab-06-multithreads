b204589d4c30cc8c9e7889e2152208df0f370900
M4skEl <sivereliseu7@gmail.com>
2020-12-29 17:00:10 +0300

    record in file

--- include/HashData.h
+++ include/HashData.h
@@ -49,31 +49,43 @@ bool is_right_hash(const std::string& str) {
 
 std::string RandData() { return std::to_string(rand()); }
 
-std::mutex mtx;
+void WriteHash(std::ofstream& out, const HashData& hd, std::string pth) {
+  out.open(pth, std::ofstream::app);
+  if (!out.is_open()) std::cout<<"Recording error"<<std::endl;
+  out << "     {\n      \"timestamp\":  " << hd.time
+      << ",\n      \"hash\": \"" << hd.hash << "\",\n      \"data\": \""
+      << hd.data << "\"\n     },\n";
+  out.close();
+}
 
+
+std::mutex mtx;
 std::vector<HashData> hashes;
 
-void FindHash() {
+void FindHash(std::string path) {
   HashData hd;
   size_t start_time = clock();
-   for (;;) {
-  hd.data = RandData();
-  picosha2::hash256_hex_string(hd.data, hd.hash);
-  if (is_right_hash(hd.hash)) {
-    size_t end_time = clock();
-    hd.time = end_time - start_time;
-    BOOST_LOG_TRIVIAL(info)
-        << "thread id" << std::this_thread::get_id() << std::endl
-        << "time: " << hd.time << std::endl
-        << "hash: " << hd.hash << std::endl
-        << "data: " << hd.data;
-    mtx.lock();
-    // Write to file
-    HashData right_hash(hd.time, hd.hash, hd.data);
-    hashes.push_back(right_hash);
-    mtx.unlock();
-    break;
-  }
+  for (;;) {
+    hd.data = RandData();
+    picosha2::hash256_hex_string(hd.data, hd.hash);
+    if (is_right_hash(hd.hash)) {
+      size_t end_time = clock();
+      hd.time = end_time - start_time;
+      BOOST_LOG_TRIVIAL(trace)
+          << std::endl
+          << "thread id" << std::this_thread::get_id() << std::endl
+          << "time: " << hd.time << std::endl
+          << "hash: " << hd.hash << std::endl
+          << "data: " << hd.data << std::endl;
+      mtx.lock();
+      // Write to file
+      std::ofstream output;
+      WriteHash(output,hd, path);
+      HashData right_hash(hd.time, hd.hash, hd.data);
+      hashes.push_back(right_hash);
+      mtx.unlock();
+      break;
+    }
   }
 }
 
